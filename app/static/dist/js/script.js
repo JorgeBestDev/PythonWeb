0ada4a844893d98223799333746d14336a1903a3
JorgeBestDev <jorgito475@gmail.com>
2024-02-07 20:16:36 -0500

    commit

--- app/static/dist/js/script.js
+++ app/static/dist/js/script.js
@@ -1,16 +1,16 @@
-const galleryContainer = document.querySelector('.gallery.container');
-const galleryControlsContainer = document.querySelector('.gallery.controls');
-const galleryControls = ['previus','next'];
-const galleryItems = document.querySelectorAll('.gallery.item');
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlContainer = document.querySelector('.gallery-control');
+const galleryControl = ['previus','next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
 
 class Carousel {
-    constructor(container, items, controls){
+    constructor(container, items, control) {
         this.carouselContainer = container;
-        this.carouselControls = controls;
+        this.carouselControl = control;
         this.carouselArray = [...items];
     }
 
-    updateGallery(){
+    updateGallery() {
         this.carouselArray.forEach(el => {
             el.classList.remove('gallery-item-1');
             el.classList.remove('gallery-item-2');
@@ -18,13 +18,38 @@ class Carousel {
             el.classList.remove('gallery-item-4');
             el.classList.remove('gallery-item-5');
         });
-        this.carouselArray.slice(0,5).forEach((el , i) => {
-            el.classList.add(`gallery-items-${i+1}`);
+        this.carouselArray.slice(0, 5).forEach((el, i) => {
+            el.classList.add(`gallery-item-${i + 1}`);
         });
     }
-    setCurrentState(direction){
-        if(direction.className == 'gallery-controls-previous'){
-            
+
+    setCurrentState(direction) {
+        if (direction.className == 'gallery-control-previus') {
+            this.carouselArray.push(this.carouselArray.shift());
+        } else {
+            this.carouselArray.unshift(this.carouselArray.pop());
         }
+        this.updateGallery();
+    }
+
+    setControl() {
+        this.carouselControl.forEach(control => {
+            galleryControlContainer.appendChild(document.createElement('button')).className = `gallery-control-${control}`;
+            document.querySelector(`.gallery-control-${control}`).innerText=control;
+        });
+    }
+
+    useControl() {
+        const triggers = [...galleryControlContainer.childNodes];
+        triggers.forEach(control => {
+            control.addEventListener('click', e => {
+                e.preventDefault();
+                this.setCurrentState(control);
+            });
+        });
     }
 }
+
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControl);
+exampleCarousel.setControl();
+exampleCarousel.useControl();
