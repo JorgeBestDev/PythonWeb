5675acc07b092e869c0181030c2ff7ae1c8009df
JorgeBestDev <jorgito475@gmail.com>
2024-02-19 08:59:40 -0500

    db

--- app/config.py
+++ app/config.py
@@ -12,7 +12,7 @@ PERMANENT_SESSION_LIFETIME = timedelta(days=365)
 #correo administrador web: administrador@tiendaonline.com
 #contraseña administrador web: W#u8@r!P5b2$qF9Z
 #contraseña hasheada scrypt:32768:8:1$l3kNvbbSLHpnZDZe$192ee8d615806f7217a2101b7c752579e2a4fc0d2032075e8b0baa1fca07b60a63b175bf80c74b9ae0738fdbb0524f2e8fc3aec6224d92d760dc17c2c804ff94
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sql5685117:lLtKytF3AK@sql5.freemysqlhosting.net:3306/sql5685117'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:BeDdh5AEHg23fGbEGca4-Dbb3ch6DC2e@viaduct.proxy.rlwy.net:15114/proyecto_formativo'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 MAIL_SERVER = 'smtp.gmail.com'
