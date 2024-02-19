9b0282e5f9bb1bcf9637fca62b43745ce88438d4
JorgeBestDev <jorgito475@gmail.com>
2024-02-19 09:15:46 -0500

    fix aside

--- app/config.py
+++ app/config.py
@@ -11,6 +11,7 @@ PERMANENT_SESSION_LIFETIME = timedelta(days=365)
 #Contraseña de usuario MySQL:@Bh!5H2$9Lqz&7pX
 #correo administrador web: administrador@tiendaonline.com
 #contraseña administrador web: W#u8@r!P5b2$qF9Z
+#consulta sql para insertar admin: insert into usuario values (1, 'Administrador', 'administrador@tiendaonline.com', 'scrypt:32768:8:1$l3kNvbbSLHpnZDZe$192ee8d615806f7217a2101b7c752579e2a4fc0d2032075e8b0baa1fca07b60a63b175bf80c74b9ae0738fdbb0524f2e8fc3aec6224d92d760dc17c2c804ff94', null, null, 42, '', '', '',1, true)
 #contraseña hasheada scrypt:32768:8:1$l3kNvbbSLHpnZDZe$192ee8d615806f7217a2101b7c752579e2a4fc0d2032075e8b0baa1fca07b60a63b175bf80c74b9ae0738fdbb0524f2e8fc3aec6224d92d760dc17c2c804ff94
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:BeDdh5AEHg23fGbEGca4-Dbb3ch6DC2e@viaduct.proxy.rlwy.net:15114/proyecto_formativo'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
