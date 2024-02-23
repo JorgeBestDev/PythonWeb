9c69362f76233446598c9744f2a662d17c141cfd
JorgeBestDev <jorgito475@gmail.com>
2024-02-23 15:53:11 -0500

    carrito full

--- app/models/carrito.py
+++ app/models/carrito.py
@@ -4,6 +4,6 @@ from app import db
 class Carrito(db.Model):
     __tablename__ = 'carrito'
     idCarrito = db.Column(db.Integer, primary_key=True)
-    pedidoForaneo = db.Column(db.Integer, db.ForeignKey('pedido.idPedido'),nullable=False)
+    pedidoForaneo = db.Column(db.Integer, db.ForeignKey('pedido.idPedido', ondelete='CASCADE'),nullable=False)
     
     
--- app/routes/carrito_routes.py
+++ app/routes/carrito_routes.py
@@ -21,19 +21,68 @@ bp = Blueprint('carrito', __name__)
 @bp.route('/carrito')
 @login_required
 def index():
-    carrito = Carrito.query.all()
     usuarioActual = current_user
     if usuarioActual.is_authenticated:
         cantidad_pedidos = Pedido.query.filter_by(usuarioForaneo=usuarioActual.idUsuario).count()
     else:
         cantidad_pedidos = 0
-    pedido = Pedido.query.filter_by(usuarioForaneo=usuarioActual.idUsuario)
-    usuarioActualId=usuarioActual.idUsuario
-    consulta = db.session.execute(text("select pedido.idPedido from pedido where pedido.usuarioForaneo=:usuarioActual"), {"usuarioActual": usuarioActualId})
-    producto = db.session.execute(text("select producto.idProducto from producto where pedido.productoForaneo=:"))
-    resultados = consulta.fetchall()
-
-    db.session.close()
-
     
-    return render_template('administrador/carrito/index.html', Carrito=carrito, cantidad_pedidos=cantidad_pedidos, resultados=carrito)
+    pedidos_usuario = Pedido.query.filter_by(usuarioForaneo=usuarioActual.idUsuario).all()
+    detalles_carritos = []
+    
+    for pedido in pedidos_usuario:
+        carrito = Carrito.query.filter_by(pedidoForaneo=pedido.idPedido).first()
+        if carrito:
+            detalles_carrito = {
+                'pedido': pedido,
+                'productos': [],
+            }
+
+            # 4. Para cada carrito, obtén los productos y sus cantidades
+            pedidos_en_carrito = Pedido.query.filter_by(idPedido=carrito.pedidoForaneo).all()
+            for pedido_carrito in pedidos_en_carrito:
+                producto = Producto.query.get(pedido_carrito.productoForaneo)
+                detalles_producto = {
+                    'nombre': producto.nombreProducto,
+                    'cantidad': pedido_carrito.cantidadPedido,
+                    'imagen': producto.nombreImagen,
+                    'precio': pedido_carrito.totalPedido,
+                }
+                detalles_carrito['productos'].append(detalles_producto)
+
+            detalles_carritos.append(detalles_carrito)
+            
+
+
+    return render_template('administrador/carrito/index.html', cantidad_pedidos=cantidad_pedidos, detalles_carritos=detalles_carritos)
+
+@bp.route('/carrito/eliminar_pedido/<int:pedido_id>', methods=['POST'])
+@login_required
+def eliminar_pedido(pedido_id):
+    pedido = Pedido.query.get_or_404(pedido_id)
+    db.session.delete(pedido)
+    db.session.commit()
+
+    return redirect(url_for('carrito.index'))
+
+@bp.route('/carrito/editar-producto/<int:pedido_id>', methods=['POST'])
+@login_required
+def editar_pedido(pedido_id):
+    if request.method == 'POST':
+        carrito = Carrito.query.filter_by(pedidoForaneo=pedido_id).first()
+
+        if carrito:
+            action = request.form.get('action')
+
+            if action == 'minus':
+                # Restar un producto al pedido
+                if carrito.pedidoForaneo.cantidadPedido > 1:
+                    carrito.pedidoForaneo.cantidadPedido -= 1
+            elif action == 'plus':
+                # Sumar un producto al pedido
+                carrito.pedidoForaneo.cantidadPedido += 1
+
+            # Guardar los cambios en la base de datos
+            db.session.commit()
+    
+    return redirect(url_for('carrito.index'))
